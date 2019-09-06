a290c9e127a6c5f0f1860feee6adb3e70ed78c21
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-06 22:33:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |festivity, supply|
-      puts " #{festivity.to_s.split('_').map {|w| w.capitalize}.join(' ') }: #{supply.join(", ")}"
+      puts "#{festivity.to_s.split('_').map {|w| w.capitalize}.join(' ') }: #{supply.join(", ")}"
     end
   end
 
